e27c5e7ac460a0c1a1d4b2352d82f683a536c552
Ankit Suthar <technoankit63@gmail.com>
2026-02-21 18:06:08 +0530

    Add files via upload

---
+++ # charcature_type_cheaker2.0.py
@@ -0,0 +1,46 @@
+print("-"*45)
+wel = ("Welcome to the char type identifier program \nEnter 'quit' for exit")
+print(wel)
+print("-"*45)
+
+def char_type():
+    char = input("\nEnter your char: ").strip()
+
+    if char.lower() == 'quit':
+        return False
+
+
+    if len(char)==1:
+    
+        if char.isupper():
+            print(f"Result: Enter {char} is 'UPPERCASE")
+
+        elif char.islower():
+            print("Result: Enter {char} is 'lowercase")
+
+        elif char.isdigit():
+            print("Result: entre {char} is 'Number")
+
+        else:
+            print("Result:enter {char} is 'Speical char")
+
+    else:
+        print("Result: Invalid {char}! Please enter single char")
+
+
+count = 0
+while count < 5:
+    status = char_type()
+    if status == False:
+            break
+    count +=1
+
+print("Program Closed , Have a great day..!")
+
+
+
+
+
+      
+
+        
